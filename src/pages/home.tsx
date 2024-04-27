6660156b24c6c99f4c427925912ec312f571f639
v-motta <viniciusmotta0806@gmail.com>
2024-04-27 17:07:25 -0300

    Refactor GitHub workflow to use Docker image environment variable and run the container on the remote server

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,9 +2,12 @@ import { ModeToggle } from "@/components/mode-toggle";
 
 function Home() {
   return (
-    <div className="absolute start-0 top-0 m-3">
-      <ModeToggle />
-    </div>
+    <>
+      <div className="absolute start-0 top-0 m-3">
+        <ModeToggle />
+      </div>
+      <p className="absolute start-1/2 top-1/2">Teste da esteira</p>
+    </>
   );
 }
 
