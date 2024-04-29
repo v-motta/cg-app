d7b16ba86be184a55bd5e048f8bbe80d94228fe1
v-motta <viniciusmotta0806@gmail.com>
2024-04-29 16:46:33 -0300

    creating protected route

--- src/configs/router.tsx
+++ src/configs/router.tsx
@@ -1,15 +1,22 @@
+import ForgotPassword from "@/pages/auth/forgot-password";
 import Login from "@/pages/auth/login";
 import Costs from "@/pages/costs";
 import Home from "@/pages/home";
 import Layout from "@/pages/layout";
+import ProtectedRoute from "@/services/ProtectedRoute";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
   { path: "/login", element: <Login /> },
+  { path: "/forgot-password", element: <ForgotPassword /> },
   {
-    path: "/", element: <Layout />, children: [
-      { path: "/dash", element: <Home /> },
-      { path: "/costs", element: <Costs /> },
+    element: <ProtectedRoute />, children: [
+      {
+        element: <Layout />, children: [
+          { path: "/", element: <Home /> },
+          { path: "/costs", element: <Costs /> },
+        ]
+      },
     ]
   },
 ]);
